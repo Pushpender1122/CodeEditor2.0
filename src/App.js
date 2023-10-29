d394331d89f7561de6fc4b5b4d61a19290254644
Pushpender <Pushpenderjangra2005@gmail.com>
2023-10-29 20:51:43 +0530

    Html,css,js Code Editor

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Code from './component/code';
 import DataContext from './component/context/datacontext'; // Assuming DataContextProvider is your context provider component
+import Home from './component/hero';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Home />}></Route>
         <Route
-          path="/"
+          path="/editor"
           element={
             <DataContext>
               <Code />
